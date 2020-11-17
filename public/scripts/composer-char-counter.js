044b12465fdb7c214f88198ba8880bfe62c0cec6
audrey-audrey <audreyuu@gmail.com>
2020-11-17 23:43:43 +0000

    Creating the tweets Part 1

--- public/scripts/composer-char-counter.js
+++ public/scripts/composer-char-counter.js
@@ -2,12 +2,25 @@
 
 $(document).ready(function() {
   // --- our code goes here ---
-  // console.log(document);
 
   // event handler for form in new-tweet section
-  $('#tweet-text').on('input', function() {
 
-    console.log(140 - $(this).val().length)
+  $('.new-tweet').on('input', function () {
+    const tweetLength = $(this).find('#tweet-text').val().length;
+    const remainingChar = 140 - tweetLength;
+
+    // Alternatively, const counter = $(this).children().children().find(".counter")
+    const counter = $(this).find(".counter")
+    // Override the text with remainingChar
+    counter.text(remainingChar)
+  
+    // if remainingchar is less than 0, add another class
+    if(remainingChar < 0) {
+      counter.addClass("tweetTooLong");
+    } else {
+      counter.removeClass("tweetTooLong")
+    }
   })
+
 
 });
